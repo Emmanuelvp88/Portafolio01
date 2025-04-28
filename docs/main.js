169b010a5a46e375355b748480f933fdbe3957fe
emmanuelvp88 <emmanuel.villalvap@outlook.es>
2025-04-27 20:45:49 -0600

    Se agrego una funcion en JS para cambiar el color de boton de whatsapp al momento que llega el scroll al fother

--- docs/main.js
+++ docs/main.js
@@ -1,23 +1,33 @@
 
 
 // Codigo para aparecer una clase para el mover el NAV
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     var divbar = this.document.querySelector("nav>div");
-    divbar.classList.toggle("abajo", window.scrollY>0);
+    divbar.classList.toggle("abajo", window.scrollY > 0);
 });
-
 
 // Codigo para aparecer el menu con el boton Movile
 const buttonBars = document.querySelector(".buttonBars");
 const navLinks = document.querySelector(".nav-links");
 
-buttonBars.addEventListener("click", () =>{
+buttonBars.addEventListener("click", () => {
     navLinks.classList.toggle("nav-links_active");
 });
-navLinks.addEventListener("click", () =>{
+navLinks.addEventListener("click", () => {
     navLinks.classList.toggle("nav-links_active");
 });
 
+
+window.addEventListener('scroll', function (){
+    const icono = document.getElementById('icono');
+
+    if((window.innerHeight + window.scrollY) >= document.body.offsetHeight){
+        icono.classList.add('en-bottom');
+    }
+    else{
+        icono.classList.remove('en-bottom');
+    }
+});
 
 // Codigo para el Scroll
 
@@ -38,4 +48,16 @@ navLinks.addEventListener("click", () =>{
 // ${document}.ready( () =>{
 //     alert("asuduoasoo");
 // });
+
+
+//     window.addEventListener('scroll', function() {
+//     const icono = document.getElementById('icono');
+//     // Verifica si ya llegaste al fondo
+//     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+//         icono.classList.add('en-bottom'); // Le agrega la clase para cambiar el color
+//     } else {
+//         icono.classList.remove('en-bottom'); // Se la quita si no estás en el fondo
+//     }
+//   });
+
 
